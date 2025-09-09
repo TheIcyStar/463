6d75b298175b1f3ec9699a81090219edd6eed726
TheIcyStar <13933661+TheIcyStar@users.noreply.github.com>
2025-09-09 00:29:44 -0400

    HW1

--- HW1/WrittenQs.typ
+++ HW1/WrittenQs.typ
@@ -21,6 +21,8 @@
 
 }
 
+= Theory problems
+
 1. _What is the primary disadvantage of using a brute-force approach like an exhaustive search for solving problems, particularly as the input size grows?_
 #answer([
   Brute-force solutions search the entire problem space. Given enough problem complexity, the problem space may grow beyond reasonable means even with relatively small input sizes.
@@ -28,7 +30,7 @@
 
 2. _In the context of greedy algorithms, what does it mean to make a "locally optimal choice"?_
 #answer([
-  While the algorithm is processing the input, it considers the current item being iterated upon and a few logically adjacent items. For example, in list-type problems, only adjacent items are considered. In graph-type problems, only the current node's neighbors are considered.
+  While the algorithm is processing the input, it considers the current item being iterated upon and a few logically adjacent items. For example, in list-type problems, only adjacent items are considered. In graph-type problems, only the current node's neighbors are considered. The algorithm then picks the best choice for that narrow set of information.
 ])
 
 
@@ -39,7 +41,7 @@
 
 4. _Why is understanding the growth of functions important when analyzing the time complexity of algorithms?_
 #answer([
-  Understanding the growth of functions gives insight into the possible resource and time requirements for running the algorithm. This lets us make better decisions on which algorithms to use to solve problems, or if it is worth running the algorithm at all.
+  Understanding the growth of functions gives insight into the possible resource and time requirements for running the algorithm. This lets us make better decisions on which algorithms to use to solve problems or if it is worth running the algorithm at all.
 ])
 
 #pagebreak()
@@ -61,7 +63,36 @@
     $
 
     Therefore we choose #h(0.5em) #box(stroke: 0.1em, outset: 0.4em, [$n=44$]) #h(0.5em) .
+])
 
+#pagebreak()
 
+= Coding section running time complexity
+For the function:
+```cpp
+int stocks_exhaustive(int prices[], int numPrices)
+```
+#answer([
 
+As there are two for loops,
+
+$n+(n-1)+(n-2)+...+2+1\
+  wide = (n(n+1)/2)\
+  wide approx n^2/2$
+
+#box(stroke: 0.1em, outset: 0.4em, [$wide = O(n^2)$])
+
+])
+
+For the function:
+```cpp
+int stocks_greedy(int prices[], int numPrices)
+```
+#answer([
+
+As the function simply iterates over the list once,
+
+#box(stroke: 0.1em, outset: 0.4em, [$ = O(n)$])
+
+The greedy solution appears to be #h(0.5em) #box(stroke: 0.1em, outset: 0.4em, [correct])#h(0.5em). It will always terminate given finite input (as it simply iterates over the input), produced correct output given my tests, and I could not find any counterexamples to disprove correctness. However these facts alone is not sufficient to show corretness.
 ])
--- HW1/hw1p1.cpp
+++ HW1/hw1p1.cpp
@@ -2,49 +2,44 @@
 //PSU account: aop5448
 
 #include <iostream>
-#include <algorithm>
+#include <climits>
 
-int stocks_exhaustive(int prices[]);
-int stocks_greedy(int prices[]);
+int stocks_exhaustive(int prices[], int numPrices);
+int stocks_greedy(int prices[], int numPrices);
 
 struct pricePair {
   int index;
   int price;
 };
 
-struct { //Reference used: https://en.cppreference.com/w/cpp/algorithm/sort.html
-  bool operator()(pricePair a, pricePair b) const { return a.price < b.price; }
-} pricePairComparitor;
-
 int main(void){
   int prices[] = {7,1,5,3,6,4};
   // int prices[] = {7,6,4,3,1};
+  int numPrices = sizeof(prices) / sizeof(prices[0]);
 
   std::cout << "Exhaustive search result: \n" <<
-               stocks_exhaustive(prices) << "\n";
+               stocks_exhaustive(prices, numPrices) << "\n";
 
   std::cout << "Greedy result: \n" <<
-               stocks_exhaustive(prices) << "\n";
+               stocks_greedy(prices, numPrices) << "\n";
 
   return 0;
 }
 
 /**
  * Exhastively searches for the best price to sell with a simple nested for loop
+ * Time complexity:
+ *    n+(n-1)+(n-2)+...+2+1 = (n(n+1)/2) ~= n^2/2
+ *      = O(n^2)
  * @param prices Array of prices for each day
  * @return profit
  */
-int stocks_exhaustive(int prices[]){
-  int pricesLength = sizeof(prices) / sizeof(prices[0]);
-  int bestBuy = -1;
-  int bestSell = -1;
+int stocks_exhaustive(int prices[], int numPrices){
   int bestProfit = 0;
 
-  for(int i=0; i < pricesLength; i++){
-    for(int j=i; j < pricesLength; j++){
+  for(int i=0; i < numPrices; i++){
+    for(int j=i+1; j < numPrices; j++){
       if(prices[j] - prices[i] > bestProfit){
-        bestBuy = i;
-        bestSell = j;
         bestProfit = prices[j] - prices[i];
       }
     }
@@ -54,42 +49,28 @@ int stocks_exhaustive(int prices[]){
 }
 
 /**
- * Finds the best profit by picking the biggest price difference and checks if the buy/sell pair is valid.
- * If invalid, continues to the next biggest price difference and repeats.
+ * Finds the best profit by walking through all prices and picking the lowest and highest available
+ * Time complexity:
+ *    = O(n)
  * @param prices Array of prices for each day
  * @return profit
  */
-int stocks_greedy(int prices[]){
-  // Add indexes to our price data, as indexes are lost after sorting
-  int pricesLength = sizeof(prices) / sizeof(prices[0]);
-  pricePair* pricePairs = new pricePair[pricesLength];
+int stocks_greedy(int prices[], int numPrices){
+  int bestBuy = INT_MAX;
+  int bestSell = -1;
+  int bestProfit = 0;
 
-  for(int i=0; i < pricesLength; i++){
-    pricePairs[i].index = i;
-    pricePairs[i].price = prices[i];
+  for(int i=0; i<numPrices; i++){
+    if(prices[i] < bestBuy){
+      bestBuy = prices[i];
+      continue;
+    }
+    if(prices[i] > bestSell && prices[i] > bestBuy){
+      bestSell = prices[i];
+      bestProfit = bestSell - bestBuy;
+      continue;
+    }
   }
 
-  //sort,
-  std::sort(0, pricesLength, pricePairComparitor);
-
-  /* Check the biggest difference pairs
-  We can get the biggest pairs by putting all of the prices like this on a 2d matrix
-  for example, with prices [1,3,4,5,6,7] and the price differences in each spot:
-
-      __7___6___5___4___3___1
-    1|  6   5   4   3   2   0
-    3|  4   3   2   1   0
-    4|  3   2   1   0
-    5|  2   1   0
-    6|  1   0
-    7|  0
-
-      _20__17__11___5___3___1   <-- Doesn't work for this, values all over the place
-    1| 19  16  10   4   2   0
-    3| 17  14   8   2   0
-    5| 15  12   6   0
-   11|  9   6   0
-   17|  3   0
-   20|  0
-  */
+  return bestProfit;
 }
